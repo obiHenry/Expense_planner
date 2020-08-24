64c5314d30c8fa571956e163ad079f34e66052a6
Element Obi <obihenry578@gmail.com>
2020-08-24 15:41:46 +0100

    modified code

--- lib/widgets/transaction-list.dart
+++ lib/widgets/transaction-list.dart
@@ -35,46 +35,48 @@ class TransactionList extends StatelessWidget {
                   margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                   elevation: 5,
                   child: ListTile(
-                    leading: 
-                    // Container(
-                    //   height: 60,
-                    //   width: 50,
-                    //   decoration: BoxDecoration(
-                    //     color: Theme.of(context).primaryColor,
-                    //     shape: BoxShape.circle,
-                    //   ),
+                    leading:
 
-                    //   child: Padding(
-                    //     padding: EdgeInsets.all(5),
-                    //     child: FittedBox(
-                    //         child: Text(
-                    //             '\$${transactions[index].amount.toStringAsFixed(2)}',
-                    //             style: TextStyle(color: Colors.white),
-                    //             ),),
-                    //   ),
-                    // ),
-                      CircleAvatar(
+                    // here u can use your own written code circleavatern
+                        // Container(
+                        //   height: 60,
+                        //   width: 50,
+                        //   decoration: BoxDecoration(
+                        //     color: Theme.of(context).primaryColor,
+                        //     shape: BoxShape.circle,
+                        //   ),
+
+                        //   child: Padding(
+                        //     padding: EdgeInsets.all(5),
+                        //     child: FittedBox(
+                        //         child: Text(
+                        //             '\$${transactions[index].amount.toStringAsFixed(2)}',
+                        //             style: TextStyle(color: Colors.white),
+                        //             ),),
+                        //   ),
+                        // ),
+                        CircleAvatar(
                       radius: 30,
-
                       child: Padding(
                         padding: EdgeInsets.all(5),
                         child: FittedBox(
-                            child: Text(
-                                '\$${transactions[index].amount.toStringAsFixed(2)}',
-                                ),),
+                          child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}',
+                          ),
+                        ),
                       ),
                     ),
-
                     title: Text(
                       transactions[index].title,
                       style: Theme.of(context).textTheme.headline6,
                     ),
-
                     subtitle: Text(
                       DateFormat.yMMMd().format(transactions[index].date),
                     ),
-
-                    trailing: IconButton(icon: Icon(Icons.delete), color: Theme.of(context).errorColor, onPressed: (){}),
+                    trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Theme.of(context).errorColor,
+                        onPressed: () {}),
                   ),
                 );
                 // return Card(
